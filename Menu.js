f2328120c46558255d13650a1d3950b6515983b5
heeju <heeju@imprimedicine.com>
2023-07-27 00:18:43 +0900

    menu with card list

--- Menu.js
+++ Menu.js
@@ -1,50 +1,157 @@
-import { Pressable, StyleSheet, Alert, Dimensions, Button, Text, View } from 'react-native';
-import { useState } from 'react';
+import { StyleSheet, Dimensions, Button, Text, View, ScrollView } from 'react-native';
+import { useState, useEffect } from 'react';
+import { Badge, Snackbar, Card, Title, Paragraph, Portal, Modal, Button as PaperButton } from 'react-native-paper';
 
 const {width: SCREEN_WIDTH} = Dimensions.get("window")
 
+const menu_data = [
+  { image: require('./assets/mm.jpg'), menu_name: 'Noodle soup', price: 1000 },
+  { image: require('./assets/mm.jpg'), menu_name: 'Noodle soup1', price: 2000 },
+  { image: require('./assets/mm.jpg'), menu_name: 'Noodle soup2', price: 3000 },
+  { image: require('./assets/mm.jpg'), menu_name: 'Noodle soup3', price: 1000 },
+  { image: require('./assets/mm.jpg'), menu_name: 'Noodle soup4', price: 1000 },
+  { image: require('./assets/mm.jpg'), menu_name: 'Noodle soup5', price: 1000 },
+  { image: require('./assets/mm.jpg'), menu_name: 'Noodle soup6', price: 1000 },
+  // Add more menu items here
+];
+const tableNum = 0;
+const customerNum = 4;
+
+const initialMenuData = menu_data.reduce((data, menuItem) => {
+  data[menuItem.menu_name] = { price: menuItem.price, count: 0 };
+  return data;
+}, {});
+
 export default function MenuScreen({ navigation }) {
+    const [menuData, setMenuData] = useState(initialMenuData);
     const [count, setCount] = useState(0);
+    const [visible, setVisible] = useState(false);
+    const [selectedItem, setSelectedItem] = useState(null);
+    const [showMessage, setShowMessage] = useState(false);
+
+  useEffect(() => {
+    if (showMessage) {
+      const timer = setTimeout(() => {
+        setShowMessage(false);
+      }, 1000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [showMessage]);
+
+    const increaseCount = () => {
+      setCount(count + 1);
+    };
+  
+    const decreaseCount = () => {
+      if (count > 0) {
+        setCount(count - 1);
+      }
+    };
+
+    const handleCardPress = (item) => {
+      setSelectedItem(item);
+      setVisible(true);
+      setCount(menuData[item.menu_name]?.count);
+    };
+
+    const setMenuCount = () => {
+      // Update the quantity for the selected item
+      setMenuData((prevData) => ({
+        ...prevData,
+        [selectedItem.menu_name]: {
+          ...prevData[selectedItem.menu_name],
+          count: count,
+        },
+      }));
+      setVisible(false);
+      setCount(0);
+      setShowMessage(true);
+    };
+
+    const totalCount = Object.values(menuData).reduce((total, item) => total + item.count, 0); 
 
     return (
       <View style={styles.container}>
-        <View sytle={styles.menuList}>
-          <View style={styles.menu}>
-            <Text>메뉴1</Text>
-            <Button title="+" onPress={() => setCount(pre => pre + 1)} />
-            <Button title="-" onPress={() => setCount(pre => pre - 1)} />
-          </View>
-          <View style={styles.menu}>
-            <Text>메뉴2</Text>
-            <Button title="+" onPress={() => setCount(pre => pre + 1)} />
-            <Button title="-" onPress={() => setCount(pre => pre - 1)} />
-          </View>
-          <View style={styles.menu}>
-            <Text>메뉴3</Text>
-            <Button title="+" onPress={() => setCount(pre => pre + 1)} />
-            <Button title="-" onPress={() => setCount(pre => pre - 1)} />
-          </View>
-          <View style={styles.menu}>
-            <Text>메뉴4</Text>
-            <Button title="+" onPress={() => setCount(pre => pre + 1)} />
-            <Button title="-" onPress={() => setCount(pre => pre - 1)} />
-          </View>
+        <View style={styles.header}>
+          <Text>테이블 번호: {tableNum}</Text>
+          <Text>인원수: {customerNum}</Text>
         </View>
-        <Text style={styles.total}>{count}</Text>
-        <Button
-          title="Go"
-          onPress={() => navigation.navigate('Pay')}
-        />
+        <ScrollView contentContainerStyle={styles.menuContainer}>
+        {menu_data.map((item, index) => (
+          <Card key={index} style={styles.menuCard} onPress={() => handleCardPress(item)}>
+            <Card.Cover source={item.image} />
+            <Card.Content>
+              <Title>{item.menu_name}</Title>
+              <Paragraph>Price: {item.price.toLocaleString()}</Paragraph>
+            </Card.Content>
+          </Card>
+        ))}
+        </ScrollView>
+        {!!totalCount && <View style={styles.footer}>
+        <PaperButton
+          mode='contained'
+          onPress={() => navigation.navigate('Cart', {menuCountData: menuData})}
+          disabled={!totalCount}
+          style={styles.cartButton}
+        >장바구니 확인 후 결제하기</PaperButton>
+          <Badge
+            style={{ position: 'absolute', top: 10, right: +70 }}
+          >{totalCount}</Badge>
+        </View>}
+        {/* Modal */}
+        <Portal>
+          <Modal visible={visible} onDismiss={() => setVisible(false)}>
+            <Card>
+              <Card.Content>
+                <Title>{selectedItem?.menu_name}</Title>
+                <Paragraph>Price: {selectedItem?.price}</Paragraph>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginVertical: 10 }}>
+                  <Button title="-" onPress={decreaseCount} />
+                  <Text>{count}</Text>
+                  <Button title="+" onPress={increaseCount} />
+            </View>
+                <PaperButton icon="plus" onPress={setMenuCount}>
+                  Add to Cart
+                </PaperButton>
+              </Card.Content>
+            </Card>
+          </Modal>
+        </Portal>
+        <Snackbar
+          visible={showMessage}
+          onDismiss={() => setShowMessage(false)}
+          action={{
+            label: 'Dismiss',
+            onPress: () => setShowMessage(false),
+          }}
+        >
+          The menu has been added to the cart.
+      </Snackbar>
       </View>
     );
   }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: "center",
+    flex: 1
+  },
+  header: {
+    width: SCREEN_WIDTH,
+    flexDirection: 'row',
+    padding: 20,
+    backgroundColor: 'red',
     justifyContent: 'space-between',
-    margin: 100,
+  },
+  menuContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    padding: 16,
+  },
+  menuCard: {
+    width: '32%',
+    marginBottom: 16,
   },
   menuList: {
     width: SCREEN_WIDTH,
@@ -57,5 +164,17 @@ const styles = StyleSheet.create({
   total: {
     fontSize: 80,
     backgroundColor: "yellow",
+  },
+  footer: {
+    position: 'relative',
+    padding: 16,
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  cartButton: {
+    flex: 1,
+    marginHorizontal: 8,
+    backgroundColor: 'blue',
+    borderRadius: 5,
   }
 });
